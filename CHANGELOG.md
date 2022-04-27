6518dca588085b8394d02987b3c830e68681f61d
Stephen Li <stephenli.ca@gmail.com>
2022-04-27 08:03:39 -0400

    Use webpack entry instead of root component to resovle root chunk

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # Vue SSR Assets Plugin Changelog
 
 This document only contains breaking changes
+
+## 0.2.0
+
+* `VueSsrAssetsRenderer` constructor now accepts `webpack.config.entry` instead of root component name. Webpack sometimes do not place the root chunk (with all the initialization JS) with the root component in large applications. This change ensures the root chunk is always resolved.
--- src/utils/findComponentDependencies.ts
+++ src/utils/findComponentDependencies.ts
@@ -1,32 +1,41 @@
 import path from 'path'
-import type { Compilation } from 'webpack'
+import type { Chunk, Compilation } from 'webpack'
 import { getComponentName } from './getComponentName'
 import { getPublicPath } from './getPublicPath'
 
 export type ComponentDependencyMap = Map<string, Set<string>>
 
 export function findComponentDependencies(compilation: Compilation): ComponentDependencyMap {
-    const componentDependencyMap: ComponentDependencyMap = new Map()
+    const dependencyMap: ComponentDependencyMap = new Map()
+    const addChunkAsDependency = (moduleName: string, chunk: Chunk) => {
+        let dependencies = dependencyMap.get(moduleName)
+        if (!dependencies) {
+            dependencies = new Set()
+            dependencyMap.set(moduleName, dependencies)
+        }
+
+        for (const file of chunk.files) {
+            const publicPath = getPublicPath(compilation, file)
+            dependencies.add(path.join(publicPath, file))
+        }
+    }
 
     for (const chunk of compilation.chunks) {
-        for (const module of compilation.chunkGraph.getChunkModules(chunk)) {
-            const componentFileName = getComponentName(module)
-            if (!componentFileName) {
-                continue
-            }
+        if (compilation.entries.has(chunk.name)) {
+            // Special case when chunk.name is defined (this is an entry chunk)
+            addChunkAsDependency(chunk.name, chunk)
+        } else {
+            // Find all modules that this chunk is generated from and add this chunk as their dependencies
+            for (const module of compilation.chunkGraph.getChunkModules(chunk)) {
+                const componentFileName = getComponentName(module)
+                if (!componentFileName) {
+                    continue
+                }
 
-            let componentFileNames = componentDependencyMap.get(componentFileName)
-            if (!componentFileNames) {
-                componentFileNames = new Set()
-                componentDependencyMap.set(componentFileName, componentFileNames)
-            }
-
-            for (const file of chunk.files) {
-                const publicPath = getPublicPath(compilation, file)
-                componentFileNames.add(path.join(publicPath, file))
+                addChunkAsDependency(componentFileName, chunk)
             }
         }
     }
 
-    return componentDependencyMap
+    return dependencyMap
 }
